423dd781646f688bf552bcc03284a643f6789918
tylertaylor <vikingtyty@gmail.com>
2017-02-28 19:41:06 -0600

    added pagination to discs controller - NOT working with the filteredList, only with the main discs list. gotta figure that out

--- app/assets/javascripts/discs/discs.controller.js
+++ app/assets/javascripts/discs/discs.controller.js
@@ -8,22 +8,36 @@
 
     function DiscsController(Auth, $rootScope, $location, $state, DiscFactory, $filter, discs) {
       var vm = this
-      vm.discs = discs
-      var POSTS_PER_PAGE = 30
+      // vm.discs = discs
+      var DISCS_PER_PAGE = 30
 
-      // vm.page = 0
-      // vm.totalDiscs = vm.discs.data.length
-      // vm.totalPages = Math.ceil(vm.totalDiscs / POSTS_PER_PAGE)
+      vm.page = 0
+      vm.totalDiscs = discs.length
+      vm.totalPages = Math.ceil(vm.totalDiscs / DISCS_PER_PAGE)
 
-
-
-      vm.search = ''
-
-      vm.refilter = function () {
-        vm.filteredList = $filter('filter')(vm.discs, vm.search)
+      vm.paginateDiscs = function () {
+        vm.discs = discs.slice(vm.page * DISCS_PER_PAGE, (vm.page + 1) * DISCS_PER_PAGE)
       }
 
-      vm.refilter()
+      vm.previousPage = function () {
+        vm.page--
+        vm.paginateDiscs()
+      }
+
+      vm.nextPage = function () {
+        vm.page++
+        vm.paginateDiscs()
+      }
+
+      vm.paginateDiscs()
+
+      // vm.search = ''
+
+      // vm.refilter = function () {
+      //   vm.filteredList = $filter('filter')(vm.discs, vm.search)
+      // }
+
+      // vm.refilter()
 
     }
 
