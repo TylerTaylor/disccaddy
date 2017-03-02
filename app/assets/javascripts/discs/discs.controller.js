278d98d3ac7e455f399629bd554b8e515fbde3f1
tylertaylor <vikingtyty@gmail.com>
2017-03-02 10:32:05 -0600

    finally got images loading, moved them to public/images... not sure why i failed so hard loading them from asset pipeline. may look into compressing file sizes for the web

--- app/assets/javascripts/discs/discs.controller.js
+++ app/assets/javascripts/discs/discs.controller.js
@@ -4,31 +4,13 @@
 
   angular
     .module('discCaddy')
-    .controller('DiscsController', ['Auth', '$rootScope', '$location', '$scope', 'DiscFactory', '$filter', 'discs', DiscsController])
+    .controller('DiscsController', ['Auth', '$rootScope', '$location', 'DiscFactory', '$filter', 'discs', DiscsController])
 
-    function DiscsController(Auth, $rootScope, $location, $scope, DiscFactory, $filter, discs) {
+    function DiscsController(Auth, $rootScope, $location, DiscFactory, $filter, discs) {
       var vm = this
       vm.discs = discs
-      // var DISCS_PER_PAGE = 30
 
       vm.page = 1
-      // vm.totalDiscs = discs.length
-      // vm.totalPages = Math.ceil(vm.totalDiscs / DISCS_PER_PAGE)
-
-      // vm.paginateDiscs = function () {
-      //   vm.discs = discs.slice(vm.page * DISCS_PER_PAGE, (vm.page + 1) * DISCS_PER_PAGE)
-      //   vm.refilter()
-      // }
-
-      // vm.previousPage = function () {
-      //   vm.page--
-      //   vm.paginateDiscs()
-      // }
-
-      // vm.nextPage = function () {
-      //   vm.page++
-      //   vm.paginateDiscs()
-      // }
 
       vm.search = ''
 
@@ -36,13 +18,6 @@
         vm.filteredList = $filter('filter')(vm.discs, vm.search)
       }
 
-      vm.pageChanged = function() {
-        var startPos = (vm.page) * 30;
-        //$scope.displayItems = $scope.totalItems.slice(startPos, startPos + 3);
-        console.log(vm.page);
-      };
-
-      // vm.paginateDiscs()
       vm.refilter()
 
     }
