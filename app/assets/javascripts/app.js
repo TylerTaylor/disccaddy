2766bcf8697ec0b47c49f92c3c70a8b8564e6cc5
tylertaylor <vikingtyty@gmail.com>
2017-03-08 09:44:24 -0600

    added dependency for ngMessages in app.js

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,7 +3,7 @@
   'use strict'
 
   angular
-    .module('discCaddy', ['templates', 'Devise', 'ui.router', 'ui.bootstrap'])
+    .module('discCaddy', ['templates', 'Devise', 'ui.router', 'ui.bootstrap', 'ngMessages'])
     .config(function($httpProvider) {
       // for CSRF errors
       $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content')
