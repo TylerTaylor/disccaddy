58523c14b71f98a31bf8925d526e0fdee937ab3e
tylertaylor <vikingtyty@gmail.com>
2017-09-08 16:48:43 -0500

    another minsafe attempt

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,13 +1,13 @@
 (function () {
-  
+
   'use strict'
 
   angular
     .module('discCaddy', ['ngMessages', 'templates', 'Devise', 'ui.router', 'ui.bootstrap', 'ngCookies'])
-    .config(function($httpProvider) {
+    .config([function($httpProvider) {
       // for CSRF errors
       $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content')
-    })
+    }])
 
   angular.module('discCaddy').run(function($rootScope, $state, Auth, $cookies) {
     $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
