7864c2130d812c32682203b242450e4b54a1e7bb
tylertaylor <vikingtyty@gmail.com>
2017-09-15 12:39:54 -0500

    added username uniqueness validation on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates_uniqueness_of :username
+
   has_many :bags
   has_many :discs, through: :bags
 end
